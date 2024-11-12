b308cad65eeb05681b474841e50578c8eb201a69
Wonder Boy <wonder725boy@gmail.com>
2024-11-13 03:12:04 +0800

    Add Multisign transaction and loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useStateStore } from "./store/stateStore.js";
-import logo from './AClogo.png';
-import './App.css';
-import copyTextToClipboard from './helpers/clipboard';
-import useSolanaWallet from './hooks/useSolanaWallet';
+import logo from "./AClogo.png";
+import "./App.css";
+import copyTextToClipboard from "./helpers/clipboard";
+import useSolanaWallet from "./hooks/useSolanaWallet";
 
 function App() {
-
   const setUseMainnet = useStateStore((state) => state.setUseMainnet);
-  const { account, connect, reset, signPersonalMessage, sendTransaction } = useSolanaWallet();
+  const {
+    account,
+    connect,
+    reset,
+    signPersonalMessage,
+    sendTransaction,
+    sendMultiSignTransaction,
+  } = useSolanaWallet();
 
-  const [ response, setResponse ] = useState('');
-  const [ text, setText ] = useState('');
-  const [ showText, setShowText ] = useState(false);
-  const [ buttonText, setButtonText ] = useState('Copy');
-  const [ showButton, setShowButton ] = useState(false);
-  const [ showDisconnectButton, setShowDisconnectButton ] = useState(false);
+  const [response, setResponse] = useState("");
+  const [text, setText] = useState("");
+  const [showText, setShowText] = useState(false);
+  const [buttonText, setButtonText] = useState("Copy");
+  const [showButton, setShowButton] = useState(false);
+  const [showDisconnectButton, setShowDisconnectButton] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const copyToClipboard = (text) => {
     copyTextToClipboard(text);
-    setButtonText('Copied');
-  }
+    setButtonText("Copied");
+  };
 
   useEffect(() => {
     const handleLoad = () => {
       const urlParams = new URLSearchParams(window.location.search);
-      const isMainnet = urlParams.get("networkId") === '0' ? true : false;
+      const isMainnet = urlParams.get("networkId") === "0" ? true : false;
       setUseMainnet(isMainnet);
 
       connect();
     };
-    window.addEventListener('load', handleLoad);
+    window.addEventListener("load", handleLoad);
     return () => {
-      window.removeEventListener('load', handleLoad);
+      window.removeEventListener("load", handleLoad);
     };
   });
 
@@ -53,44 +60,70 @@ function App() {
 
     if (action === "sign" && message) {
       return signMessage(message);
-    }
-    else if (action === "send" && txMessage) {
+    } else if (action === "send" && txMessage) {
       return sendRawTransaction(txMessage);
+    } else if (action === "sendMultiSign" && txMessage) {
+      return sendRawTransaction(txMessage, true);
     }
     displayResponse("Invalid URL");
   }
 
   async function signMessage(message) {
+    setIsLoading(true);
     let signature;
     try {
       signature = await signPersonalMessage(message);
     } catch (error) {
+      setIsLoading(false);
     }
+    setIsLoading(false);
 
     if (signature) {
       console.log({ signature });
-      if (message === "Please sign this message to log in with your wallet address.")
-        displayResponse("Signature complete.\n\nCopy to clipboard and sign the next message to login", `${account}-${signature}`);
+      if (
+        message ===
+        "Please sign this message to log in with your wallet address."
+      )
+        displayResponse(
+          "Signature complete.\n\nCopy to clipboard and sign the next message to login",
+          `${account}-${signature}`
+        );
       else
-        displayResponse("Signature complete.\n\nCopy to clipboard and return to Astro Café", `${account}-${signature}`);
+        displayResponse(
+          "Signature complete.\n\nCopy to clipboard and return to Astro Café",
+          `${account}-${signature}`
+        );
     } else {
-      displayResponse("Signature Denied.\n\nCopy to clipboard and return to Astro Café", "error");
+      displayResponse(
+        "Signature Denied.\n\nCopy to clipboard and return to Astro Café",
+        "error"
+      );
     }
   }
 
-  async function sendRawTransaction(txMessage) {
+  async function sendRawTransaction(txMessage, multiSign = false) {
+    setIsLoading(true);
     let txHash;
     try {
-      txHash = await sendTransaction(txMessage);
+      txHash = multiSign
+        ? await sendMultiSignTransaction(txMessage)
+        : await sendTransaction(txMessage);
     } catch (error) {
       console.error(error);
     }
+    setIsLoading(false);
 
     if (txHash) {
       console.log({ txHash });
-      displayResponse("Transaction sent.\n\nCopy to clipboard and return to Astro Café", txHash);
+      displayResponse(
+        "Transaction sent.\n\nCopy to clipboard and return to Astro Café",
+        txHash
+      );
     } else {
-      displayResponse("Transaction Denied.\n\nCopy to clipboard and return to Astro Café", "error");
+      displayResponse(
+        "Transaction Denied.\n\nCopy to clipboard and return to Astro Café",
+        "error"
+      );
     }
   }
 
@@ -108,14 +141,34 @@ function App() {
   return (
     <div id="app">
       <div className="header">
-        <button id="disconnect-button" title="Disconnect wallet" className={showDisconnectButton ? 'active': ''} onClick={() => reset()}>X</button>
+        <button
+          id="disconnect-button"
+          title="Disconnect wallet"
+          className={showDisconnectButton ? "active" : ""}
+          onClick={() => reset()}
+        >
+          X
+        </button>
       </div>
       <div className="center">
         <div id="logo">
           <img src={logo} alt="logo" />
         </div>
-        <div id="response-text" className={showText ? 'active': ''}>{text}</div>
-        <button id="response-button" className={showButton ? 'active': ''} onClick={() => copyToClipboard(response)}>{buttonText}</button>
+        {isLoading && (
+          <div className="spinner">
+            <div className="loading"></div>
+          </div>
+        )}
+        <div id="response-text" className={showText ? "active" : ""}>
+          {text}
+        </div>
+        <button
+          id="response-button"
+          className={showButton ? "active" : ""}
+          onClick={() => copyToClipboard(response)}
+        >
+          {buttonText}
+        </button>
       </div>
     </div>
   );
--- src/hooks/useSolanaWallet.js
+++ src/hooks/useSolanaWallet.js
@@ -1,40 +1,45 @@
 /* eslint-disable */
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { VersionedMessage, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
+import {
+  VersionedMessage,
+  TransactionMessage,
+  VersionedTransaction,
+  Transaction,
+} from "@solana/web3.js";
 import bs58 from "bs58";
-import { hexToByte } from '../helpers/string';
-import { sleep } from '../helpers/time';
+import { Buffer } from "buffer";
+import { hexToByte } from "../helpers/string";
+import { sleep } from "../helpers/time";
 
 const useSolanaWallet = () => {
-
   const wallet = useWallet();
   const walletModal = useWalletModal();
   const { connection } = useConnection();
 
-  const [account, setAccount] = useState('');
+  const [account, setAccount] = useState("");
 
   useEffect(() => {
-    setAccount(wallet?.publicKey ? wallet.publicKey.toBase58() : '');
+    setAccount(wallet?.publicKey ? wallet.publicKey.toBase58() : "");
 
     if (wallet.connected && walletModal.visible) {
       walletModal.setVisible(false);
     }
   }, [wallet]);
 
-  const connect = async() => {
+  const connect = async () => {
     if (!wallet.connected) {
       walletModal.setVisible(true);
     }
-  }
+  };
 
-  const reset = async() => {
-    await wallet.disconnect()
-  }
+  const reset = async () => {
+    await wallet.disconnect();
+  };
 
-  const signPersonalMessage = async(message) => {
+  const signPersonalMessage = async (message) => {
     if (account && wallet?.signMessage) {
       const data = new TextEncoder().encode(message);
       const signature = await wallet.signMessage(data);
@@ -42,21 +47,21 @@ const useSolanaWallet = () => {
       return serializedSignature;
     }
     return null;
-  }
+  };
 
-  const sendTransaction = async(txMessage) => {
+  const sendTransaction = async (txMessage) => {
     if (account && wallet.publicKey) {
       const data = hexToByte(txMessage);
       const message = VersionedMessage.deserialize(data);
-
       const {
         context: { slot: minContextSlot },
-        value: { blockhash },
+        value: { blockhash, lastValidBlockHeight },
       } = await connection.getLatestBlockhashAndContext();
-      const lastValidBlockHeight = minContextSlot + 150;
       message.recentBlockhash = blockhash;
       const transactionMessage = TransactionMessage.decompile(message);
-      const transaction = new VersionedTransaction(transactionMessage.compileToLegacyMessage());
+      const transaction = new VersionedTransaction(
+        transactionMessage.compileToLegacyMessage()
+      );
 
       let signedTx;
       if (wallet.signTransaction) {
@@ -68,24 +73,47 @@ const useSolanaWallet = () => {
 
       if (signedTx) {
         const rawTransaction = signedTx.serialize();
-        let blockheight = await connection.getBlockHeight();
-        do {
-          const txHash = await connection.sendRawTransaction(rawTransaction, { skipPreflight: true });
-          await sleep(500);
-          const ret = await connection.getTransaction(txHash, {commitment: "confirmed", maxSupportedTransactionVersion: 1});
-          if (ret) {
-            return txHash;
-          }
-          blockheight = await connection.getBlockHeight();
-          console.log(`retrying transfer if blockheight(${blockheight}) less than ${lastValidBlockHeight}`);
-        } while (blockheight < lastValidBlockHeight);
+        const txHash = await connection.sendRawTransaction(rawTransaction, {
+          skipPreflight: false,
+          preflightCommitment: "confirmed",
+        });
+        await connection.confirmTransaction(txHash, "confirmed");
+        return txHash;
       } else {
-        const txHash = await wallet.sendTransaction(transaction, connection, { minContextSlot });
+        const txHash = await wallet.sendTransaction(transaction, connection, {
+          minContextSlot,
+        });
         return txHash;
       }
     }
     return null;
-  }
+  };
+
+  const sendMultiSignTransaction = async (txMessage) => {
+    if (account && wallet.publicKey) {
+      const transaction = Transaction.from(Buffer.from(txMessage, "hex"));
+
+      let signedTx;
+      if (wallet.signTransaction) {
+        signedTx = await wallet.signTransaction(transaction);
+      } else if (wallet.signAllTransactions) {
+        const signedAllTxs = await wallet.signAllTransactions([transaction]);
+        signedTx = signedAllTxs[0];
+      }
+
+      if (signedTx) {
+        const rawTransaction = signedTx.serialize();
+        const txHash = await connection.sendRawTransaction(rawTransaction, {
+          skipPreflight: false,
+          preflightCommitment: "confirmed",
+        });
+        await connection.confirmTransaction(txHash, "confirmed");
+        return txHash;
+      }
+      return null;
+    }
+    return null;
+  };
 
   return {
     account,
@@ -93,7 +121,8 @@ const useSolanaWallet = () => {
     reset,
     signPersonalMessage,
     sendTransaction,
+    sendMultiSignTransaction,
   };
-}
+};
 
 export default useSolanaWallet;
